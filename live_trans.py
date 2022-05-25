9fef35d20cccf4e01427f82e163332552958408c
ch-suginami <ch.suginami.dp@gmail.com>
2022-05-25 21:24:04 +0900

    DeepL無料版に対応

--- live_trans.py
+++ live_trans.py
@@ -1,33 +1,27 @@
-import traceback
-import locale
-import time
+# -*- coding: utf-8 -*-
+
 import datetime
 import dateutil.parser
-import requests
 import json
-import sys
+import requests
 import regex
-
-# -*- coding: utf-8 -*-
+import sys
+import time
+import traceback
 
 # how to compile execution style
 # pyinstaller live_trans.py --onefile
 
 # API Key Information
-YT_API_KEY_BACK_UP = "AIzaSyCcnPvxJ9nm5vABHOAjxt0WDGUvbxefp7s"
-DL_API_KEY = "5ebc8721-eb64-797d-d9bd-6e3bf637ceac"
 JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
-
-get_sleep = 3  # seconds
 
 # translating with DeepL
 
 
-def dl_trans(msg):
+def dl_trans(DL_API_KEY, URL, msg):
     params = {'auth_key': DL_API_KEY, 'text': msg, 'target_lang': 'JA'}
 
-    data = requests.post(
-        'https://api.deepl.com/v2/translate', data=params).json()
+    data = requests.post(URL, data=params).json()
 
     return(data['translations'][0]['text'])
 
@@ -35,7 +29,6 @@ def dl_trans(msg):
 def get_chat_id(yt_url, yt_api):
     # getting video_id
     video_id = yt_url.replace('https://www.youtube.com/watch?v=', '')
-    #print(f'video_id: {video_id}')
 
     # requetsing chat ID
     url = 'https://www.googleapis.com/youtube/v3/videos'
@@ -47,80 +40,65 @@ def get_chat_id(yt_url, yt_api):
         liveStreamingDetails = data['items'][0]['liveStreamingDetails']
     except:
         return False
+
     if 'activeLiveChatId' in liveStreamingDetails.keys():
         chat_id = liveStreamingDetails['activeLiveChatId']
-        # print(chat_id)
     else:
         chat_id = None
-        print('Not Active Live')
+        print('ライブはオフラインです')
 
     return chat_id
 
 
-def get_chat(chat_id, pageToken, f_today, yt_api, cnt):
+def get_chat(chat_id, pageToken, f_today, yt_api, deepl_API_key, cnt, DL_URL):
     # setting parameters
     url = 'https://www.googleapis.com/youtube/v3/liveChat/messages'
-    params = {'key': yt_api, 'liveChatId': chat_id,
-              'part': 'id, snippet, authorDetails'}
+    params = {'key': yt_api, 'liveChatId': chat_id,'part': 'id, snippet, authorDetails'}
+
     if type(pageToken) == str:
         params['pageToken'] = pageToken
     else:
-        print("Something wrong to get params.")
+        print("パラメータ指定にエラーがあります")
         sys.exit()
 
     # requesting chat itself
     data = requests.get(url, params=params).json()
 
-    # print(data)
-
     with open(f_today, 'a', encoding='utf-8') as f:
-        if cnt == 0 and yt_api == YT_API_KEY_BACK_UP:
-            f.write(f'The API key is back up.\n')
-        elif cnt == 0:
-            f.write(f"The API key is streamer's.\n")
         # repeating for checking comments
         try:
             for item in data['items']:
-                #channelId = item['snippet']['authorChannnelId']
                 msg = item['snippet']['displayMessage']
                 usr = item['authorDetails']['displayName']
-                c_time = dateutil.parser.parse(item['snippet']['publishedAt']).astimezone(
-                    JST).strftime("%Y-%m-%d %H:%M:%S")
-                #supStic = item['snippet']['superStickerDetails']
-                #usr = str_left(25, usr)
+                c_time = dateutil.parser.parse(item['snippet']['publishedAt']) \
+                    .astimezone(JST).strftime("%Y-%m-%d %H:%M:%S")
                 # checking Japanese(Hiragana or Katakana)
                 if not (regex.search(r'{\p{Katakana}+}', msg) and regex.search(r'{\p{Hiragana}+}', msg)):
                     # if not included, throw translation
-                    trans = dl_trans(msg)
+                    trans = dl_trans(deepl_API_key, DL_URL, msg)
                 else:
                     # if included, it is Japanese and not throw translation
                     trans = msg
+
                 # main output part
                 # checking term 'superChat' in item list
                 if item.get('superChatDetails'):
                     supChatAmount = item['snippet']['superChatDetails']['amountDisplayString']
                     supChatComment = item['snippet']['superChatDetails']['userComment']
-                    print(
-                        f'{usr}さんからスパチャが来ました！ナイスパ！！ : {usr} Thanks for superchat!')
+                    print(f'{usr}さんからスパチャが来ました！ナイスパ！！ : {usr} Thanks for superchat!')
                     print(f'{supChatAmount}!')
                     print(f'  original : {supChatComment}')
                     print(f'  Japanese : {trans}')
                     print('')
-                    f.write(
-                        f'{c_time}:\n{usr}:\n  金額 : {supChatAmount}\n  元コメント : {supChatComment}\n  翻訳コメント : {trans}\n\n')
+                    f.write(f'{c_time}:\n{usr}:\n  金額 : {supChatAmount}\n  元コメント : {supChatComment}\n  翻訳コメント : {trans}\n\n')
                     f.flush()
                 else:
                     print(f'{usr}:')
                     print(f'  original : {msg}')
                     print(f'  Japanese : {trans}')
                     print('')
-                    f.write(
-                        f'{c_time}:\n{usr}:\n  original : {msg}\n  Japanese : {trans}\n\n')
+                    f.write(f'{c_time}:\n{usr}:\n  original : {msg}\n  Japanese : {trans}\n\n')
                     f.flush()
-
-                # time.sleep(get_sleep)
-                #print('start : {data["items"][0]["snippet"]["publishedAt"]}')
-                #print('end   : {data["items"][-1]["snippet"]["publishedAt"]}')
 
         except:
             traceback.print_exc()
@@ -132,8 +110,39 @@ def get_chat(chat_id, pageToken, f_today, yt_api, cnt):
     return data['nextPageToken']
 
 
-def main(yt_url, yt_api):
-    slp_time = 5  # in seconds
+def main():
+    with open('./API/key.txt', 'r') as f:
+        try:
+            yt_api = f.readline().replace("YouTubeAPIKey=", "").strip()
+            deepl_api = f.readline().replace("DeepLAPIKey=", "").strip()
+            slp_time = int(f.readline().replace("チャット取得時間間隔=").strip())
+        except:
+            print("key.txtの記述が不正です。ファイルを再確認してください。")
+            sys.exit()
+
+    while True:
+        print(u'YouTubeLiveアドレスを入力してください。')
+        yt_url = input()
+        # checking right Live address
+        if not regex.match(r'^https://www.youtube.com/watch', yt_url):
+            print('正しいYouTubeアドレスを指定してください。（https://www.youtube.com/watchから始まっていますか？）')
+        else:
+            break
+
+    # checking which key of DeepL API use
+    # Try to use paying key
+    params = {'auth_key': deepl_api, 'text': "Live streamings will help us happy!", 'target_lang': 'JA'}
+    check = requests.post('https://api.deepl.com/v2/translate', data=params).json()
+
+    if check == "<Response [403]>":
+        try:
+            check = requests.post('https://api-free.deepl.com/v2/translate', data=params).json()
+        except:
+            print("DeepL APIキーの値が正しくありません。")
+            sys.exit()
+        DL_URL = 'https://api-free.deepl.com/v2/translate'
+    else:
+        DL_URL = 'https://api.deepl.com/v2/translate'
 
     chat_id = get_chat_id(yt_url, yt_api)
 
@@ -143,44 +152,15 @@ def main(yt_url, yt_api):
     nextPageToken = ''
     count = 0
 
-    # adding counter to write API key information
-
     # infinity loop
     while(chat_id):
         try:
-            nextPageToken = get_chat(
-                chat_id, nextPageToken, f_today, yt_api, count)
+            nextPageToken = get_chat(chat_id, nextPageToken, f_today, yt_api, deepl_api, count, DL_URL)
             time.sleep(slp_time)
         except:
             break
         count += 1
 
-    chat_id = get_chat_id(yt_url, YT_API_KEY_BACK_UP)
-    count2 = 0
-
-    while(chat_id):
-        try:
-            nextPageToken = get_chat(
-                chat_id, nextPageToken, f_today, YT_API_KEY_BACK_UP, count2)
-            time.sleep(slp_time)
-        except Exception as e:
-            print(e)
-            # print('何らかの理由により、コメントの取得が終了しました。')
-            print('何かキーを押してください。')
-            input()
-            break
-        count2 += 1
-
 
 if __name__ == '__main__':
-    with open('./API/key.txt', 'r') as f:
-        yt_api = f.readline()
-    while True:
-        print(u'YouTubeLiveアドレスを入力してください。')
-        yt_url = input()
-        # checking right Live address
-        if not regex.match(r'^https://www.youtube.com/watch', yt_url):
-            print('正しいYouTubeアドレスを指定してください。（https://www.youtube.com/watchから始まっていますか？）')
-        else:
-            break
-    main(yt_url, yt_api)
+    main()
